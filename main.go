982c22b2f6f98a65fb678aebaf96a160823b8708
srikrishnavamsi <srikrishnavamsi@gmail.com>
2020-12-10 16:38:03 +0000

    testing main

--- main.go
+++ main.go
@@ -1,34 +1,40 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	
+	// "encoding/json"
+  // "io/ioutil"
+  // "os"
   "fmt"
   "github.com/uswitch/segment-config-go/segment"
-  "os"
   // "reflect"
 )
 
 
 func main() {
-  client := segment.NewClient("t5dwvaonRp-UqFZJX0iOStP1Cj8_Ea25z8FuxDQWL8U.sHdro1niwYG3oqrnVxSgBUEYNdNPUNVvNiRRUutCHbc", "uswitch-sandbox")
-  
-  jsonFile, err := os.Open("segment/trackingplan.json")
+
+  workspace := "uswitch-sandbox"
+  accessToken := "t5dwvaonRp-UqFZJX0iOStP1Cj8_Ea25z8FuxDQWL8U.sHdro1niwYG3oqrnVxSgBUEYNdNPUNVvNiRRUutCHbc"
+  client, err := segment.NewClient(&accessToken, &workspace)
 
   if err != nil {
     fmt.Println(err)
   }
-
-  fmt.Println("successfully opened json file")
-  defer jsonFile.Close()
-  byteValue, _ := ioutil.ReadAll(jsonFile)
-
-  var result map[string]interface{}
-  json.Unmarshal([]byte(byteValue), &result)
+  fmt.Println(client)
   
-  tp, err := client.CreateTrackingPlan(result)
+  // jsonFile, err := os.Open("segment/trackingplan.json")
 
-  fmt.Println(tp)
+  // if err != nil {
+  //   fmt.Println(err)
+  // }
 
+  // fmt.Println("successfully opened json file")
+  // defer jsonFile.Close()
+  // byteValue, _ := ioutil.ReadAll(jsonFile)
+
+  // var result map[string]interface{}
+  // json.Unmarshal([]byte(byteValue), &result)
+  
+  // tp, err := client.CreateTrackingPlan(result)
+
+  // fmt.Println(tp)
 }
