5afcc6dd30363623fe24fbba0ad0d7e589371c4e
Andreas Williams <andreas.williams@rvu.co.uk>
2021-03-03 18:07:59 +0000

    Make variables to create client required

--- segment/client.go
+++ segment/client.go
@@ -23,37 +23,18 @@ type Client struct {
 	apiVersion  string
 	accessToken string
 	workspace   string
-	HTTPClient      *http.Client
+	HTTPClient  *http.Client
 }
 
 // NewClient creates a new Segment Config API client.
-func NewClient(accessToken, workspace *string) (*Client, error) {
-	c := Client{
+func NewClient(accessToken, workspace string) *Client {
+	return &Client{
 		baseURL:     defaultBaseURL,
 		apiVersion:  apiVersion,
-		accessToken: *accessToken,
-		workspace:   *workspace,
-		HTTPClient:      http.DefaultClient,
+		accessToken: accessToken,
+		workspace:   workspace,
+		HTTPClient:  http.DefaultClient,
 	}
-
-	// check if accessToken and workspace are valid
-	if (accessToken != nil) && (workspace != nil) {
-		endpoint := "workspaces/uswitch-sandbox"
-		
-		body, err := c.doRequest(http.MethodGet, endpoint, nil)
-		
-		if err != nil {
-			return nil, err
-		}
-
-		wr := Workspace{}
-		
-		err = json.Unmarshal(body, &wr)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return &c, nil
 }
 
 func (c *Client) doRequest(method, endpoint string, data interface{}) ([]byte, error) {
@@ -97,7 +78,6 @@ func (c *Client) doRequest(method, endpoint string, data interface{}) ([]byte, e
 	case http.StatusNotFound:
 		return nil, fmt.Errorf("the requested uri does not exist")
 	case http.StatusBadRequest:
-		
 		return nil, fmt.Errorf("the request is invalid")
 	default:
 		return nil, fmt.Errorf("bad response code: %d", resp.StatusCode)
